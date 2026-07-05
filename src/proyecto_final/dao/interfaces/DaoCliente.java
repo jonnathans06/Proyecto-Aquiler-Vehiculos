3e86569affaffb1c2eddedd3b11e6b4fc0c0ec26
jonnathans06 <LENOVO@Lenovo-de-Jonna>
2026-07-05 16:55:47 -0500

    Se creo la funcionalidad de buscar y listar clientes

--- src/proyecto_final/dao/interfaces/DaoCliente.java
+++ src/proyecto_final/dao/interfaces/DaoCliente.java
@@ -4,9 +4,9 @@ import java.util.List;
 import proyecto_final.modelo.Cliente;
 
 public interface DaoCliente {
-    boolean crearCliente(Cliente cliente);    
-    Cliente buscarClientePorCedula(String cedula);
-    List<Cliente> buscarPorCoincidencia(String nombre);
+    boolean crearCliente(Cliente cliente);
+    Cliente buscarClientePorCedula(String busqueda);
+    List<Cliente> buscarClientes(String busqueda);
     List<Cliente> listarTodods();
     boolean actualizarCliente(Cliente cliente);
     boolean eliminarCliente(Cliente cliente);
